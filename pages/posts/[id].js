aa5ed2d20b273d8fdce3aa6fe3b4f3c93d1e5c27
Joseph Martire <joemart06@gmail.com>
2023-03-24 15:17:03 -0400

    Add post params; Add fs as a package, did not fix the fs issue

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,23 @@
 import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
 
-export default function Post() {
+export async function getStaticProps({params}){
+    const postData = getPostData(params.id)
+
+    return {
+        props: {
+            postData
+        }
+    }
+}
+
+export default function Post({postData}) {
   return <Layout>
     
-
+    <div>{postData.title}</div>
+    <div>{postData.id}</div>
+    <div>{postData.date}</div>
+    
 
   </Layout>;
 }
